99cbf54be5133c99e573d894c9b9c7ed77bcda7c
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-26 16:01:04 +0530

    create a service for song class

--- module-02-Creating-REST-APIS/Lesson-02/src/app.module.ts
+++ module-02-Creating-REST-APIS/Lesson-02/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { SongService } from './song/song.service';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SongService],
 })
 export class AppModule {}
