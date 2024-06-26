3442e288fe470705d0798de439e712435a10f3ec
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-26 15:03:10 +0530

    updated crud

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('songs')
-export class SongsController {}
+export class SongsController {
+    
+    @Post()
+    create() {
+        return 'create a new song endpoint';
+    }
+
+    @Get()
+    findAll(){
+        return ' find all songs endpoint';
+    }
+
+    @Get(':id')
+    findOne(){
+        return 'find one song endpoint';
+    }
+
+    @Put(':id')
+    update(){
+        return 'update one song details endpoint';
+    }
+
+    @Delete(':id')
+    delete(){
+        return ' delete a specific song endpoint';
+    }
+}
