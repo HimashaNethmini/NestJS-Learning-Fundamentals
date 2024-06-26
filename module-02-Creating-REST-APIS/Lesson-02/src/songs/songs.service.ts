66937da1d620fc1c26a31504ce8c24f9804a2e78
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-26 16:42:01 +0530

    implement database services

--- module-02-Creating-REST-APIS/Lesson-02/src/songs/songs.service.ts
+++ module-02-Creating-REST-APIS/Lesson-02/src/songs/songs.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SongsService {}
+export class SongsService {
+    //local db
+    //local array
+
+    private readonly songs = [];
+
+    //create crud
+    create(song){
+        this.songs.push(song);
+        return this.songs;
+    }
+
+    //get crud
+    findAll(){
+        //fetch from database
+        return this.songs;
+    }
+
+}
