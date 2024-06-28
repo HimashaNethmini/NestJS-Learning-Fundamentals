9d44068c9168a65947d95fd3a926051a9f5c6e11
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-28 16:46:44 +0530

    validators

--- module-02-Creating-REST-APIS/Lesson-02/src/songs/dto/create-songs/create-songs.ts
+++ module-02-Creating-REST-APIS/Lesson-02/src/songs/dto/create-songs/create-songs.ts
@@ -1,1 +1,23 @@
-export class CreateSongs {}
+import { IsArray, IsDateString, IsMilitaryTime, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateSongs {
+    //making database shape
+
+    @IsString()
+    @IsNotEmpty()
+    readonly title: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsArray()
+    readonly artists: string[];
+
+    @IsNotEmpty()
+    @IsDateString()
+    readonly releaseDate: Date;
+
+    @IsMilitaryTime()
+    @IsNotEmpty()
+    readonly duration: Date;
+
+}
