0110ecb58590274bfa251d68fca7210dae720e96
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-01 12:11:35 +0530

    adding error handling to fetch all function

--- module-02-Creating-REST-APIS/Lesson-02/src/songs/songs.controller.ts
+++ module-02-Creating-REST-APIS/Lesson-02/src/songs/songs.controller.ts
@@ -1,33 +1,53 @@
-import { Controller, Get, Delete,Post, Put, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Delete,
+  Post,
+  Put,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { CreateSongs } from './dto/create-songs/create-songs';
 
 @Controller('songs')
 export class SongsController {
   //writing a constructor to fetch from service
-  constructor(private songsService: SongsService){}
+  constructor(private songsService: SongsService) {}
 
-    @Post()
-    create(@Body() CreateSongs:CreateSongs) {
-      return this.songsService.create(CreateSongs);
-    }
-    @Get()
-    findAll() {
+  @Post()
+  create(@Body() CreateSongs: CreateSongs) {
+    return this.songsService.create(CreateSongs);
+  }
+  //adding error handling to fetch all function
+  @Get()
+  findAll() {
+    try {
       return this.songsService.findAll();
-    }
-    @Get(':id')
-    findOne() {
-      return 'fetch song on the based on id';
-    }
-  
-    @Put(':id')
-    update() {
-      return 'update song on the based on id';
-    }
-  
-    @Delete(':id')
-    delete() {
-      return 'delete song on the based on id';
+    } catch (e) {
+      throw new HttpException(
+        'server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
     }
   }
 
+  @Get(':id')
+  findOne() {
+    return 'fetch song on the based on id';
+  }
+
+  @Put(':id')
+  update() {
+    return 'update song on the based on id';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'delete song on the based on id';
+  }
+}
